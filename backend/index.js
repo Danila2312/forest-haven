bb54109373a7110576736f4110fa3f56f9ddc9a8
Danila2312 <d.chistiakow2016@yandex.ru>
2024-06-16 23:26:40 +0300

    Доделал backend...

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,10 @@ const sequelize = require('./db')
 const models = require('./models/models')
 const { Sequelize } = require('sequelize')
 const cors = require('cors')
+const fileUploader = require('express-fileupload')
 const router = require('./routes/index')
 const errorHandler = require('./middleware/ErrorHandlingMiddleware')
+const path = require('path')
 
 
 const PORT = process.env.PORT || 5000
@@ -15,6 +17,8 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.resolve(__dirname,'static')))
+app.use(fileUploader({}))
 app.use('/api', router)
 
 //Отработка ошибок
--- backend/models/models.js
+++ backend/models/models.js
@@ -1,51 +1,89 @@
-const sequelize = require('../db')
-const {DataTypes}  = require('sequelize')
+const sequelize = require("../db");
+const { DataTypes } = require("sequelize");
 
-const User = sequelize.define('user', {
-  id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  email: {type: DataTypes.STRING, unique:true},
-  password: {type: DataTypes.STRING},
-  role: {type: DataTypes.STRING, defaultValue:"USER"}
-})
+const User = sequelize.define("user", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  email: { type: DataTypes.STRING, unique: true },
+  password: { type: DataTypes.STRING },
+  role: { type: DataTypes.STRING, defaultValue: "USER" },
+});
 
-const Booking = sequelize.define('booking', {
-  id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  user_id: {type: DataTypes.INTEGER, primaryKey: true,},
-  start_book: {type: DataTypes.DATE },
-  end_book: {type: DataTypes.DATE },
-})
+const Basket = sequelize.define("basket", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+});
 
-const House = sequelize.define('house', {
-  id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-  name: {type: DataTypes.STRING, allowNull: false},
-  img: {type: DataTypes.STRING, unique:true},
-  price: {type: DataTypes.INTEGER, allowNull: false},
-  rating: {type: DataTypes.INTEGER, allowNull: false} ,
-  review: {type: DataTypes.STRING, allowNull: false} ,
-} )
+const BasketDevice = sequelize.define("basket_device", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+});
 
-const Rating = sequelize.define('rating', {
-  id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  user_id: {type: DataTypes.INTEGER, primaryKey: true,  },
-  house_id: {type: DataTypes.INTEGER, primaryKey: true, },
-  rating: {type: DataTypes.INTEGER, allowNull: false} ,
-})
+const Device = sequelize.define("device", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, unique: true, allowNull: false },
+  price: { type: DataTypes.INTEGER, allowNull: false },
+  rating: { type: DataTypes.INTEGER, defaultValue: 0 },
+  img: { type: DataTypes.STRING, allowNull: false },
+});
 
-User.hasOne(Booking)
-Booking.belongsTo(User)
+const Type = sequelize.define("type", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, unique: true, allowNull: false },
+});
 
-User.hasMany(Rating)
-Rating.belongsTo(User)
+const Brand = sequelize.define("brand", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, unique: true, allowNull: false },
+});
 
-House.hasMany(Booking)
-Booking.belongsTo(House)
+const Rating = sequelize.define("rating", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  rate: { type: DataTypes.INTEGER, allowNull: false },
+});
 
-House.hasMany(Rating)
-Rating.belongsTo(House)
+const DeviceInfo = sequelize.define("device_info", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  title: { type: DataTypes.STRING, allowNull: false },
+  description: { type: DataTypes.STRING, allowNull: false },
+});
+
+const TypeBrand = sequelize.define("type_brand", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+});
+
+User.hasOne(Basket);
+Basket.belongsTo(User);
+
+User.hasMany(Rating);
+Rating.belongsTo(User);
+
+Basket.hasMany(BasketDevice);
+BasketDevice.belongsTo(Basket);
+
+Type.hasMany(Device);
+Device.belongsTo(Type);
+
+Brand.hasMany(Device);
+Device.belongsTo(Brand);
+
+Device.hasMany(Rating);
+Rating.belongsTo(Device);
+
+Device.hasMany(BasketDevice);
+BasketDevice.belongsTo(Device);
+
+Device.hasMany(DeviceInfo, { as: "info" });
+DeviceInfo.belongsTo(Device);
+
+Type.belongsToMany(Brand, { through: TypeBrand });
+Brand.belongsToMany(Type, { through: TypeBrand });
 
 module.exports = {
   User,
-  House,
-  Booking,
-  Rating
-}
+  Basket,
+  BasketDevice,
+  Device,
+  Type,
+  Brand,
+  Rating,
+  TypeBrand,
+  DeviceInfo,
+};
